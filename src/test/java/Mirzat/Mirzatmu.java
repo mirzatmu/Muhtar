15d987ad5e19613af6aeae8584ea41da3469e0a5
Mirzat <mirzatmu@gmail.com>
2019-02-20 17:20:57 -0500

    new

---
+++ src/test/java/Mirzat/Mirzatmu.java
@@ -0,0 +1,9 @@
+package Mirzat;
+
+public class Mirzatmu {
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/test/java/Muhtar/muhtarMahmut.java
+++ src/test/java/Muhtar/muhtarMahmut.java
@@ -1,7 +1,7 @@
 package Muhtar;
 
 public class muhtarMahmut {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
     }
 }
